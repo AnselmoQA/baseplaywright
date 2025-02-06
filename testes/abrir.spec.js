ebb0c7b978b689a4ba0d09bef2d711af6ad4eb19
anselmoqa <anselmo.oliveira.qa@gmail.com>
2025-02-06 12:50:30 -0300

    Primeiro commit

---
+++ testes/abrir.spec.js
@@ -0,0 +1,7 @@
+// tests/example.spec.js
+const { test, expect } = require('@playwright/test');
+
+test('Verifica título da página', async ({ page }) => {
+  await page.goto('https://example.com');
+  await expect(page).toHaveTitle('Example Domain');
+});
